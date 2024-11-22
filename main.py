4581dc370c4f61fc8301f78aa469f1292f0fafb6
camden678 <camdenj@andrew.cmu.edu>
2024-11-21 20:35:03 -0500

    11/21/24 20:00

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 from cmu_graphics import *
 from testFile import *
+import math
+from urllib.request import urlopen
+from PIL import Image
 
-def main():
-    pass
+def loadPilImageLocal(path):
+    return Image.open(path)
+
+#This function was taken from the PIL demo through the 15-112 website:
+def loadPilImageURL(url):
+
+    # Loads a PIL image (not a CMU image!) from a url:
+    return Image.open(urlopen(url))
 
 def onAppStart(app):
     app.width = 1440 #Mac screen width
     app.height = 840 #Mac screen height
     app.controlScreenHeight = 150
     app.displayXY  = (0, 0)
-    constructControls()
+    constructControls(app)
+    constructAnimals(app)
+    constructBackground(app)
     app.setMaxShapeCount(5000)
-    app.compartmentPixelSize = 15
-    app.compartmentMatrix = dict()
-    constructCompartmentMatrix(app)
-    
+    app.colorAtPix = ()
+    app.sineWave, app.sineDegrees = 0, 0
+   
+def constructBackground(app):
+    app.landscapeImage = loadPilImageLocal("/Users/camdenjohnson/Desktop/Python workspace/landscape.png")
+    app.landscapeImageRGB = app.landscapeImage.convert('RGB')
 
-def drawControls(app):
-    drawRect(0, app.height-app.controlScreenHeight, app.width, app.controlScreenHeight, fill = "blue")
-    for slider in Slider.getSliders():
-        slider.drawSlider()
-    for button in Button.getButtons():
-        button.drawButton()
-
-    
-
-def constructControls():
-    Button(100, 100, 100, 100, "red", "Red")
+def constructControls(app):
+    Button(1000, 600, 100, 100, "red", "Red")
     Slider(100, 700, "Number of Fish")
     Slider(250, 700, "Number of Poops")
     Slider(400, 700, "Number of Fish")
     Slider(650, 700, "Number of Poops")
 
+def constructAnimals(app):
+    Salmon(700, 520)
+    Salmon(500, 500)
+    
 class Slider:
     listSliders = []
     radius = 10
@@ -90,43 +98,114 @@ class Button:
             return True
         return False
 
-class Compartment:
-    listCompartments = []
-
 class Animal:
     pass
-   # listAnimals = []
-  #  def __init__(self,)
+
+class Salmon(Animal):
+
+    salmonScale = 4
+    salmonImageRightFull = loadPilImageLocal("/Users/camdenjohnson/Desktop/Python workspace/salmon.png")
+    salmonWidth, salmonHeight = salmonImageRightFull.size
+    salmonImageRight = salmonImageRightFull.resize((salmonWidth//salmonScale, salmonHeight//salmonScale))
+    salmonImageLeftFull = loadPilImageLocal("/Users/camdenjohnson/Desktop/Python workspace/salmonF.png")
+    salmonImageLeft = salmonImageLeftFull.resize((salmonWidth//salmonScale, salmonHeight//salmonScale))
+    salmonList = []
+    swimStepSizeX, swimStepSizeY = 2, 2
+
+    def __init__(self, x, y):
+        Salmon.salmonList.append(self)
+        self.leftX, self.leftY = x, y
+        self.movingRight = True
+        
+    def isInWater(self, app):
+        colorTolerance = 5
+        r, g, b = app.landscapeImageRGB.getpixel((self.leftX, self.leftY))
+        r1, g1, b1 = 46, 109, 246 #This is the blue color that is being used in the testLandscape
+        checkLeft = isSameColor(r, g, b, r1, g1, b1, colorTolerance)
+        r, g, b = app.landscapeImageRGB.getpixel((self.leftX + Salmon.salmonWidth, self.leftY))
+        checkRight = isSameColor(r, g, b, r1, g1, b1, colorTolerance)
+
+        if checkLeft and checkRight:
+            return True
+        return False
+
+    def drawSalmon(self):
+        if self.movingRight:
+            drawImage(CMUImage(Salmon.salmonImageRight), self.leftX, self.leftY)
+        else:
+            drawImage(CMUImage(Salmon.salmonImageLeft), self.leftX, self.leftY)
+
+    def swimStep(self, app):
+        self.leftY += app.sineWave * Salmon.swimStepSizeX
+        if self.movingRight:
+            self.leftX += Salmon.swimStepSizeX
+        else:
+            self.leftX -= Salmon.swimStepSizeX
+
+        if not self.isInWater(app) and self.movingRight:
+            self.movingRight = False
+        elif not self.isInWater(app) and not self.movingRight:
+            self.movingRight = True
+
+    @staticmethod
+    def getSalmonList():
+        return Salmon.salmonList
 
 def redrawAll(app):
     drawControls(app)
-    constructCompartmentMatrix(app)
-    
+    drawImage("/Users/camdenjohnson/Desktop/Python workspace/landscape.png", 0, 0)
+    for salmon in Salmon.getSalmonList():
+        salmon.drawSalmon()
+
+    drawUtils(app)
+
+def drawUtils(app):   
+    drawRect(0, 0, 100, 40, fill = "white")
 
     drawLabel(str(app.displayXY), 40, 10)
+    drawLabel(str(app.colorAtPix), 40, 30)
+    if app.colorAtPix != ():
+        r, g, b, = app.colorAtPix
+        drawRect(90, 30, 10, 10, fill = rgb(r, g, b))
 
-
-def constructCompartmentMatrix(app):
-    for x in range(0, app.width, app.compartmentPixelSize):
-        for y in range(0, app.height - app.controlScreenHeight, app.compartmentPixelSize):
-            app.compartmentMatrix[(x, y)] = None
-    
-def drawCompartmentMatrix(app):
-    for coordinate in app.compartmentMatrix:
-        x, y = coordinate
-        drawRect(x, y, app.compartmentPixelSize, app.compartmentPixelSize, fill = "red")
-
+def drawControls(app):
+    drawRect(0, app.height-app.controlScreenHeight, app.width, app.controlScreenHeight, fill = "blue")
+    for slider in Slider.getSliders():
+        slider.drawSlider()
+    for button in Button.getButtons():
+        button.drawButton()
 
 def onMouseMove(app, mouseX, mouseY):
     app.displayXY = (mouseX, mouseY) #Just a useful utlility to show location 
+    #im = Image.open('pond.jpg') # Can be many different formats.
+    #pix = im.load()
+    try:
+        app.colorAtPix = app.landscapeImageRGB.getpixel((mouseX, mouseY))  # Get the RGBA Value of the a pixel of an image
+    except:
+        pass
 
 def onMouseDrag(app, mouseX, mouseY):
     Slider.updateSliders(mouseX, mouseY)
 
+def onStep(app):
+    app.sineDegrees +=1
+    if app.sineDegrees == 360:
+        app.sineDegrees = 0
+    app.sineWave = math.sin(app.sineDegrees*10)
+    for salmon in Salmon.getSalmonList():
+        salmon.swimStep(app)
+
 def distance(x1, y1, x2, y2):
+
     return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 
-main()
+def isSameColor(r, g, b, r1, g1, b1, colorTolerance):
+    if abs(r1-r) <= colorTolerance and \
+        abs(g1-g) <= colorTolerance and \
+        abs(b1-b) <= colorTolerance:
+        return True
+    return False
+
 runApp()
 
 
